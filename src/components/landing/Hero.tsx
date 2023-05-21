55df04ff55125450539d6bd606b5ade3d08d0225
aeriaIs <nosvengeance@gmail.com>
2023-05-21 14:24:08 +0700

    update component colors

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,23 +1,21 @@
+import { ShopIllustration } from "../../utils/images.js";
+
 export const Hero = () => {
   return (
     <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
       <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
         <svg
-          className="absolute left-0 hidden h-full text-blue-900/80 transform -translate-x-1/2 lg:block"
+          className="absolute left-0 hidden h-full text-[#70d5e4]/30 transform -translate-x-1/2 lg:block"
           viewBox="0 0 100 100"
           fill="currentColor"
           preserveAspectRatio="none slice">
           <path d="M50 0H100L50 100H0L50 0Z" />
         </svg>
-        <img
-          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
-          src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-          alt=""
-        />
+        <img className="transition-all ease-in-out duration-300 object-contain w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full" src={ShopIllustration} alt="" />
       </div>
       <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
         <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-          <p className="inline-block px-2.5 py-0.5 mb-4 text-xs font-semibold tracking-wide text-teal-900 uppercase rounded-full bg-teal-400">
+          <p className="inline-block px-2.5 py-0.5 mb-4 text-xs font-semibold tracking-wide bg-sky-300 text-teal-900 uppercase rounded-full">
             Brand new
           </p>
           <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl sm:leading-none">
@@ -26,8 +24,8 @@ export const Hero = () => {
             can imagine <span className="inline-block text-deep-purple-accent-400">is real</span>
           </h2>
           <p className="pr-5 mb-5 text-base text-gray-400 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-            aperiam, eaque ipsa quae. explicabo.
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
+            explicabo.
           </p>
           <div className="flex items-center">
             <a
