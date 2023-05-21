29bc5a0f6884838964fae6f2a1e997fcb34529b5
aeriaIs <nosvengeance@gmail.com>
2023-05-21 14:23:48 +0700

    added tw color template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main": "#0891b2",
+        "secondary": "#22d3ee"
+      }
+    },
   },
   plugins: [],
 };
