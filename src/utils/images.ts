6a9b7671feb2b9835dadf53744d0da66777ad820
aeriaIs <nosvengeance@gmail.com>
2023-05-21 20:54:32 +0700

    added icons

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,8 @@
 import ShopIllustration from "../assets/undraw_shop.svg";
+import AboutCompanyIllustration from "../assets/about_company.svg";
 
-export { ShopIllustration };
+import ConvenientIcon from "../assets/convenient.png";
+import CustomerSupportIcon from "../assets/help_desk.png";
+import ProductIcon from "../assets/new_product.png";
+
+export { ShopIllustration, AboutCompanyIllustration, ConvenientIcon, CustomerSupportIcon, ProductIcon };
